329e4da72e3dcecf2119cf771dfc29fe9b99eb03
Andrew Best <andrew.best@tri.global>
2021-11-18 16:36:51 -0800

    Add launch file to set bridge passive and create the object sensor

---
+++ carla_ros_bridge/launch/carla_ros_passive_sensing.launch.py
@@ -0,0 +1,92 @@
+from pathlib import Path
+
+from ament_index_python.packages import get_package_share_directory
+import launch
+from launch.substitutions import TextSubstitution
+import launch_ros.actions
+
+
+def generate_launch_description():
+    # Get the object spawner node which creates the sensors we want.
+    object_spawn_path = Path(get_package_share_directory("carla_spawn_objects"))
+    included_launch = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            str((object_spawn_path / "launch" / "carla_spawn_objects.launch.py").absolute())
+        ),
+        launch_arguments={
+            "objects_definition_file": TextSubstitution(
+                text=str((object_spawn_path / "config" / "object_sensor_only.json").absolute())
+            ),
+            "spawn_sensors_only": TextSubstitution(text="true"),
+        }.items(),
+    )
+    ld = launch.LaunchDescription(
+        [
+            included_launch,
+            launch.actions.DeclareLaunchArgument(
+                name="host", default_value="localhost", description="IP of the CARLA server"
+            ),
+            launch.actions.DeclareLaunchArgument(
+                name="port", default_value="2000", description="TCP port of the CARLA server"
+            ),
+            launch.actions.DeclareLaunchArgument(
+                name="timeout",
+                default_value="2",
+                description="Time to wait for a successful connection to the CARLA server",
+            ),
+            launch.actions.DeclareLaunchArgument(
+                name="synchronous_mode",
+                default_value="True",
+                description="Enable/disable synchronous mode. If enabled, the ROS bridge waits until the expected data is received for all sensors",
+            ),
+            launch.actions.DeclareLaunchArgument(
+                name="fixed_delta_seconds",
+                default_value="0.05",
+                description="Simulation time (delta seconds) between simulation steps",
+            ),
+            launch.actions.DeclareLaunchArgument(
+                name="ego_vehicle_role_name",
+                default_value=[
+                    "hero",
+                    "ego_vehicle",
+                    "hero0",
+                    "hero1",
+                    "hero2",
+                    "hero3",
+                    "hero4",
+                    "hero5",
+                    "hero6",
+                    "hero7",
+                    "hero8",
+                    "hero9",
+                ],
+                description="Role names to identify ego vehicles. ",
+            ),
+            launch_ros.actions.Node(
+                package="carla_ros_bridge",
+                executable="bridge",
+                name="carla_ros_bridge",
+                output="screen",
+                emulate_tty="True",
+                on_exit=launch.actions.Shutdown(),
+                parameters=[
+                    # TODO(andrew.best): Perhaps this solves our problems w.r.t to time?
+                    {"use_sim_time": True},
+                    {"host": launch.substitutions.LaunchConfiguration("host")},
+                    {"port": launch.substitutions.LaunchConfiguration("port")},
+                    {"timeout": launch.substitutions.LaunchConfiguration("timeout")},
+                    {"passive": True},
+                    {"synchronous_mode": launch.substitutions.LaunchConfiguration("synchronous_mode")},
+                    {"synchronous_mode_wait_for_vehicle_control_command": False},
+                    {"fixed_delta_seconds": launch.substitutions.LaunchConfiguration("fixed_delta_seconds")},
+                    {"register_all_sensors": True},
+                    {"ego_vehicle_role_name": launch.substitutions.LaunchConfiguration("ego_vehicle_role_name")},
+                ],
+            ),
+        ]
+    )
+    return ld
+
+
+if __name__ == "__main__":
+    generate_launch_description()
--- carla_spawn_objects/setup.py
+++ carla_spawn_objects/setup.py
@@ -1,50 +1,50 @@
 """
 Setup for carla_spawn_objects
 """
-import os
 from glob import glob
-ROS_VERSION = int(os.environ['ROS_VERSION'])
+import os
+from pathlib import Path
+
+ROS_VERSION = int(os.environ["ROS_VERSION"])
 
 if ROS_VERSION == 1:
     from distutils.core import setup
     from catkin_pkg.python_setup import generate_distutils_setup
 
-    d = generate_distutils_setup(
-        packages=['carla_spawn_objects'],
-        package_dir={'': 'src'}
-    )
+    d = generate_distutils_setup(packages=["carla_spawn_objects"], package_dir={"": "src"})
 
     setup(**d)
 
 elif ROS_VERSION == 2:
     from setuptools import setup
 
-    package_name = 'carla_spawn_objects'
+    package_name = "carla_spawn_objects"
+    package_dir = Path(".")
+    share_root = "share/{}".format(package_name)
+    share_config = "{}/config".format(share_root)
+    share_launch = "{}/launch".format(share_root)
+
     setup(
         name=package_name,
-        version='0.0.0',
-        packages=['src/' + package_name],
+        version="0.0.0",
+        packages=["src/" + package_name],
         data_files=[
-            ('share/ament_index/resource_index/packages',
-             ['resource/' + package_name]),
-            ('share/' + package_name, ['package.xml']),
-            ('share/' + package_name + '/config',
-             ['config/objects.json']),
-            ('share/' + package_name + '/config',
-             ['config/object_sensor_only.json']),
-            (os.path.join('share', package_name), glob('launch/*.launch.py'))
+            ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+            (share_root, ["package.xml"]),
+            (share_config, [str(item) for item in (package_dir / "config").glob("*.json")]),
+            (share_launch, [str(item) for item in (package_dir / "launch").glob("*.launch*")]),
         ],
-        install_requires=['setuptools'],
+        install_requires=["setuptools"],
         zip_safe=True,
-        maintainer='CARLA Simulator Team',
-        maintainer_email='carla.simulator@gmail.com',
-        description='CARLA spawn_objects for ROS2 bridge',
-        license='MIT',
-        tests_require=['pytest'],
+        maintainer="CARLA Simulator Team",
+        maintainer_email="carla.simulator@gmail.com",
+        description="CARLA spawn_objects for ROS2 bridge",
+        license="MIT",
+        tests_require=["pytest"],
         entry_points={
-            'console_scripts': [
-                'carla_spawn_objects = src.carla_spawn_objects.carla_spawn_objects:main',
-                'set_initial_pose = src.carla_spawn_objects.set_initial_pose:main'
-            ],
+            "console_scripts": [
+                "carla_spawn_objects = src.carla_spawn_objects.carla_spawn_objects:main",
+                "set_initial_pose = src.carla_spawn_objects.set_initial_pose:main",
+            ]
         },
     )
